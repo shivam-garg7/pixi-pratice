df11022dcf193ebcf794bb3dd507e87d166d3626
Shivam Garg <shivamgarg2232001@gmail.com>
2022-09-16 03:43:40 +0530

    commit

--- sep14/src/Game.ts
+++ sep14/src/Game.ts
@@ -22,101 +22,100 @@ export class Game extends Application {
     this.loader.load(cb);
   }
   /////----------------------------------------------------------------------------------
-//    rever():any{
+   rever():any{
    
    
-//     const spineboy = new Spine(getResource("hero").spineData);
-//      spineboy.x = this.screen.width/2+300;
-//      spineboy.y=this.screen.height;
+    const spineboy = new Spine(getResource("hero").spineData);
+     spineboy.x = this.screen.width/2+300;
+     spineboy.y=this.screen.height;
     
-//      spineboy.scale.set(1);
-//      spineboy.skeleton.setSlotsToSetupPose();
-//      spineboy.stateData.setMix('walk', 'jump', 0.2);
-//      spineboy.stateData.setMix('jump', 'walk', 0.4);
-//      spineboy.state.setAnimation(0, "idle", true);
-//      this.stage.addChild(spineboy);
-//      let previousAnim1: string = 'idle';
-//      let currentAnim1: string = 'idle';
-//      let revertToAnim1: string = 'idle';
-//      let direction1: number = 1;
-//      let defaultScale1 = spineboy.scale.x;
-//      this.ticker.add(() => {
-//        spineboy.scale.x = defaultScale1 * direction1;
-//        if (currentAnim1 != previousAnim1) {
-//          console.log(currentAnim1, previousAnim1);
-//          spineboy.state.setAnimation(0, currentAnim1, true);
-//          previousAnim1 = currentAnim1;
+     spineboy.scale.set(1);
+     spineboy.skeleton.setSlotsToSetupPose();
+     spineboy.stateData.setMix('walk', 'jump', 0.2);
+     spineboy.stateData.setMix('jump', 'walk', 0.4);
+     spineboy.state.setAnimation(0, "idle", true);
+     this.stage.addChild(spineboy);
+     let previousAnim1: string = 'idle';
+     let currentAnim1: string = 'idle';
+     let revertToAnim1: string = 'idle';
+     let direction1: number = 1;
+     let defaultScale1 = spineboy.scale.x;
+     this.ticker.add(() => {
+       spineboy.scale.x = defaultScale1 * direction1;
+       if (currentAnim1 != previousAnim1) {
+         console.log(currentAnim1, previousAnim1);
+         spineboy.state.setAnimation(0, currentAnim1, true);
+         previousAnim1 = currentAnim1;
  
-//        }
-//      });
+       }
+     });
      
-//      function onKeyEvent(e: KeyboardEvent): void {
-//        console.log("keyboard event", e);
-//        switch (e.type) {
-//          case "keyup":
-//            switch (e.code) {
-//              default:
-//                currentAnim1 = revertToAnim1;
-//                spineboy.y=this.screen.height;
-//                // direction = 1;
-//                break;
-//            }
-//            break;
-//          case "keydown":
-//            switch (e.code) {
-//              case 'KeyW':
-//                currentAnim1 = 'jump';
-//                spineboy.y=320;
-//                break;
-//              case 'KeyS':
-//                currentAnim1 = 'walk';
-//                revertToAnim1 = 'walk';
-//                direction1 = 1;
-//                break;
-//                case 'KeyK':
-//                  currentAnim1 = 'fall';
-//                  revertToAnim1 = 'idle';
-//                  // direction = 1;
-//                  break;
-//                case 'KeyL':
-//                  currentAnim1 = 'crouch';
-//                  revertToAnim1 = 'walk';
-//                  // direction = 1;
-//                  break;
-//                  case 'KeyP':
-//                    currentAnim1 = 'run';
-//                    revertToAnim1 = 'walk';
-//                    // direction = 1;
-//                    break;
-//              case 'KeyM':
-//                currentAnim1 = 'attack';
-//                revertToAnim1 = 'idle';
-//                direction1 = 1;
-//                break;
-//              case 'KeyN':
-//                currentAnim1 = 'walk';
-//                revertToAnim1 = 'walk';
-//                direction1 = -1;
-//                break;
-//              case 'keyB':
-//                currentAnim1 = 'attack';
-//                break;
-//              default:
-//                currentAnim1 = 'idle';
-//                revertToAnim1 = 'idle';
-//            }
-//            break;
-//          default:
-//            console.warn("Event has no listener", e.type);
-//        }
-//      }
-//      window.addEventListener("keydown", onKeyEvent);
-//      window.addEventListener("keyup", onKeyEvent);
-//    }
-//   /////////////////-----------------------------------------------------------------------
+     function onKeyEvent(e: KeyboardEvent): void {
+       console.log("keyboard event", e);
+       switch (e.type) {
+         case "keyup":
+           switch (e.code) {
+             default:
+               currentAnim1 = revertToAnim1;
+               // direction = 1;
+               break;
+           }
+           break;
+         case "keydown":
+           switch (e.code) {
+             case 'KeyW':
+               currentAnim1 = 'jump';
+               spineboy.y=300;
+               break;
+             case 'KeyS':
+               currentAnim1 = 'walk';
+               revertToAnim1 = 'walk';
+               direction1 = 1;
+               break;
+               case 'KeyK':
+                 currentAnim1 = 'fall';
+                 revertToAnim1 = 'idle';
+                 // direction = 1;
+                 break;
+               case 'KeyL':
+                 currentAnim1 = 'crouch';
+                 revertToAnim1 = 'walk';
+                 // direction = 1;
+                 break;
+                 case 'KeyP':
+                   currentAnim1 = 'run';
+                   revertToAnim1 = 'walk';
+                   // direction = 1;
+                   break;
+             case 'KeyM':
+               currentAnim1 = 'attack';
+               revertToAnim1 = 'idle';
+               direction1 = 1;
+               break;
+             case 'KeyN':
+               currentAnim1 = 'walk';
+               revertToAnim1 = 'walk';
+               direction1 = -1;
+               break;
+             case 'keyB':
+               currentAnim1 = 'attack';
+               break;
+             default:
+               currentAnim1 = 'idle';
+               revertToAnim1 = 'idle';
+           }
+           break;
+         default:
+           console.warn("Event has no listener", e.type);
+       }
+     }
+     window.addEventListener("keydown", onKeyEvent);
+     window.addEventListener("keyup", onKeyEvent);
+   }
+  /////////////////-----------------------------------------------------------------------
   onLoad(): void {
     this.stage.interactive = true;
-    // this.rever();
+    this.rever();
 ///-------------------------------------------------------------------------------
       const gob = new Spine(getResource("goblin").spineData);
     gob.skeleton.setSkinByName("goblin");
